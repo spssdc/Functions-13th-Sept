c7023d02959f438edda3cd92db73b5a5e3349e98
sdc <sdc@SPSICT217-TEMP>
2022-09-13 10:55:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Functions_Example
+{
+    class Program
+    {
+        static int square (int n)
+        {
+            int result;
+
+            result = n * n;
+            return result;
+        }
+
+        static void message(string e)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(e);
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Square 2 " + square(2));
+            message("Extra stuff");
+        }
+    }
+}
